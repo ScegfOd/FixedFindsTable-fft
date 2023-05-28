0bfdbbefcac0a2b5c98017c28f9fb3e5965b7092
Jonathan Combs <JofCombs@gmail.com>
2023-05-28 18:30:03 -0400

    fake cookie accept?

--- app/scraper.py
+++ app/scraper.py
@@ -10,6 +10,7 @@ Index one contains a datetime object referring to the Saturday after the Friday 
 	headers = {
 		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36",
    		"X-Requested-With": "XMLHttpRequest",
+   		"cookie": "value stored in your webpage",
    	}
 	table_df = pd.read_html("http://www.csueastbay.edu/students/academics-and-studying/finals/"+season+".html", header = headers)[0]
 
